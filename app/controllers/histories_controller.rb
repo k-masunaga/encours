966d1d8b4dd867f5bc455fc8e496a249320dde73
k-masunaga <masumath84@gmail.com>
2021-08-09 14:58:16 +0900

    全履歴の表示

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,4 +1,7 @@
 class HistoriesController < ApplicationController
   def show
+    @spendings = Spending.all.where(user_id: current_user.id)
+    @incomes = Income.all.where(user_id: current_user.id)
+    # @all = User.Joins(:spending, :income).all.where(user_id: current_user.id)
   end
 end
