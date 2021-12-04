944e7e1cdca4afa9451a2fb6f52bf25d0e2dc1e2
k-masunaga <masumath84@gmail.com>
2021-12-04 14:52:51 +0900

    検索機能追加

--- app/controllers/analyses_controller.rb
+++ app/controllers/analyses_controller.rb
@@ -1,11 +1,27 @@
 class AnalysesController < ApplicationController
+  before_action :set_q, only: [:index, :search, :show]
+  def index
+  end
+
   def show
     spendings = Spending.joins(:spending_category).all.where(user_id: current_user.id)
     incomes = Income.joins(:income_category).all.where(user_id: current_user.id)
+
+    @spending_category = SpendingCategory.all
 
     @total = spendings | incomes
     @total.sort!{ |spendings, incomes| incomes.date <=> spendings.date }
 
     @dw = ["日", "月", "火", "水", "木", "金", "土"]
   end
+
+  def search
+    @results = @q.result
+  end
+
+  private
+  def set_q
+    @q = Spending.ransack(params[:q])
+  end
+
 end
