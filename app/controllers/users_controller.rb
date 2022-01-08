b649da2cd7ff2e7e9833cf18d427e78de4ec1789
k-masunaga <masumath84@gmail.com>
2022-01-08 16:40:19 +0900

    円グラフの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,15 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
+    @this_year = @month.year
+    @this_month = @month.month
+    @sdg_orders = Spending.where(date: @month.all_month)
+    @icm_orders = Income.where(date: @month.all_month)
+
+    @ratio = @sdg_orders.joins(:spending_category).group("name").sum(:price).sort_by { |_, v| v }.reverse.to_h
+
   end
 
   def delete
