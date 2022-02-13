e153ae892523202681044f06e6f9d0b4491ba907
k-masunaga <masumath84@gmail.com>
2022-02-13 16:17:21 +0900

    最近の入力、カレンダーの表示変更

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -1,4 +1,6 @@
 class InputsController < ApplicationController
+  before_action :set_beginning_of_week, only: [:show, :calendar]
+
   def show
     spendings = Spending.joins(:spending_category).all.where(user_id: current_user.id)
     incomes = Income.joins(:income_category).all.where(user_id: current_user.id)
@@ -6,9 +8,14 @@ class InputsController < ApplicationController
     @samples = spendings | incomes
     @samples.sort!{ |spendings, incomes| incomes.created_at <=> spendings.created_at }
 
-    @dw = ["日", "月", "火", "水", "木", "金", "土"]
   end
 
   def calendar
+  end
+
+  private
+  def set_beginning_of_week
+    Date.beginning_of_week = :sunday
+    @dw = ["日", "月", "火", "水", "木", "金", "土"]
   end
 end
